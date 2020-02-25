580143a04900ca627a062c766aae237efe9fce11
victor <vicross615@gmail.com>
2020-02-25 12:51:18 +0100

    basic authentication entry point

--- src/main/java/admin/portal/constant/AppConstants.java
+++ src/main/java/admin/portal/constant/AppConstants.java
@@ -2,4 +2,5 @@ package admin.portal.constant;
 
 public class AppConstants {
      public static final int MAX_ALLOWED_INACTIVE_SESSION_MIN= 5;
+    public static final String RESPONSE_MESSAGE_KEY = "message";
 }
--- src/main/java/admin/portal/models/BasicAuthenticationEntryPoint.java
+++ src/main/java/admin/portal/models/BasicAuthenticationEntryPoint.java
@@ -8,6 +8,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+
+import static admin.portal.constant.AppConstants.RESPONSE_MESSAGE_KEY;
 
 public class BasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private Gson gson;
@@ -23,5 +26,24 @@ public class BasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse httpServletResponse,
                          AuthenticationException e) throws IOException, ServletException {
 
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.addHeader("Content-type", "Application/json");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, POST, HEAD,DELETE, PATCH");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, X-Csrf-Token, WWW-Authenticate, Authorization");
+
+
+        httpServletResponse.getWriter().println(this.gson.toJson(new HashMap<String, String>() {{
+            put(RESPONSE_MESSAGE_KEY, "invalid credential");
+        }}));
     }
+
+
+
+
+
+
+
+
 }
